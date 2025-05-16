a4145ce28c46e414803ccf61a9655a33c908a8d0
PaterneSeka1 <sekapaterne25@gmail.com>
2025-05-16 08:13:22 +0000

    work of yersterday

--- server.js
+++ server.js
@@ -38,3 +38,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
 });
+
+// add a new models(models Post and models Comments)
